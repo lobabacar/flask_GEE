ac70ac6acee04e66117db314df8cbde71e0f1547
lobabacar <babacar.toure.lo@usherbrooke.ca>
2026-03-15 19:51:08 -0400

    Initialize Earth Engine inside NDVI route

--- app/routes.py
+++ app/routes.py
@@ -62,9 +62,51 @@ def index():
 
 
 
+# @main.route("/get_ndvi_tile", methods=["POST"])
+# def get_ndvi_tile():
+#     try:
+#         data = request.get_json()
+#         selected_date = data.get("date")
+
+#         if not selected_date:
+#             return jsonify({"error": "Date manquante"}), 400
+
+#         start_date = datetime.strptime(selected_date, "%Y-%m-%d")
+#         end_date = start_date + timedelta(days=1)
+
+#         point = ee.Geometry.Point([-17.44, 14.69])
+
+#         collection = (
+#             ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED")
+#             .filterBounds(point)
+#             .filterDate(start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d"))
+#             .sort("CLOUDY_PIXEL_PERCENTAGE")
+#         )
+
+#         count = collection.size().getInfo()
+#         if count == 0:
+#             return jsonify({"error": "Aucune image trouvée pour cette date"}), 404
+
+#         image = collection.first()
+#         ndvi = ee.Image(image).normalizedDifference(["B8", "B4"]).rename("NDVI")
+
+#         map_id = ndvi.getMapId({
+#             "min": 0,
+#             "max": 1,
+#             "palette": ["blue", "white", "green"]
+#         })
+
+#         return jsonify({
+#             "tile_url": map_id["tile_fetcher"].url_format
+#         })
+
+#     except Exception as e:
+#         return jsonify({"error": str(e)}), 500
 @main.route("/get_ndvi_tile", methods=["POST"])
 def get_ndvi_tile():
     try:
+        init_gee()
+
         data = request.get_json()
         selected_date = data.get("date")
 
